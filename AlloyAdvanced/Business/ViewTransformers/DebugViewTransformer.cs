463f3c18b224ecec0f2b1491ccf0e3c2c3418aa2
Artur Vasyliev <princeartik@gmail.com>
2020-08-17 18:23:09 +0300

    Implemented custom view transformers
    Implemented custom view transformers which allow debugging and removing components for specific users.

---
+++ AlloyAdvanced/Business/ViewTransformers/DebugViewTransformer.cs
@@ -0,0 +1,39 @@
+﻿using EPiServer.Shell.ViewComposition;
+using System;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace AlloyAdvanced.Business.ViewTransformers
+{
+    [ViewTransformer]
+    public class DebugViewTransformer : IViewTransformer
+    {
+        public int SortOrder => 0;
+
+        public void TransformView(ICompositeView view, IPrincipal principal)
+        {
+            Debug.WriteLine($"View: {view.Name}, Title: {view.Title}, {DateTime.Now}");
+            ScanViewRecursively(view.RootContainer, "  ");
+        }
+        private void ScanViewRecursively(IContainer container, string indent)
+        {
+            Debug.WriteLine($"{indent}Container: {container.DefinitionName}, {container.WidgetType}");
+
+            indent += "  ";
+
+            foreach (IComponent component in container.Components)
+            {
+                // if this component is a container then scan it
+                IContainer containerComponent = component as IContainer;
+                if (containerComponent != null)
+                {
+                    ScanViewRecursively(containerComponent, indent);
+                }
+                else
+                {
+                    Debug.WriteLine($"{indent}Component: {component.DefinitionName}, {component.WidgetType}");
+                }
+            }
+        }
+    }
+}
---
+++ AlloyAdvanced/Business/ViewTransformers/RemoveComponentsViewTransformer.cs
@@ -0,0 +1,83 @@
+﻿using EPiServer.Shell.ViewComposition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+
+namespace AlloyAdvanced.Business.ViewTransformers
+{
+    [ViewTransformer]
+    public class RemoveComponentsViewTransformer : IViewTransformer
+    {
+        private string userName = "Alice";
+
+        private string[] componentsToRemove = new string[]
+        {
+            "EPiServer.Cms.Shell.UI.Components.ProjectModeToolbarComponent",
+            "EPiServer.Cms.Shell.UI.Components.RecentItems"
+        };
+
+        public int SortOrder => 0;
+
+        public void TransformView(ICompositeView view, IPrincipal principal)
+        {
+            // build a list of components to remove
+            var components = new List<IComponentMatcher>();
+            BuildListRecursively(view.RootContainer, components);
+
+            // remove the components for the specified user
+            if (string.Equals(userName, principal.Identity.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                view.RootContainer.RemoveComponentsRecursive(components,
+                    notifyComponentOnRemoval: false);
+            }
+        }
+
+        private void BuildListRecursively(IContainer container, List<IComponentMatcher> components)
+        {
+            foreach (IComponent component in container.Components)
+            {
+                IContainer childContainer = component as IContainer;
+                if (childContainer != null)
+                {
+                    BuildListRecursively(childContainer, components);
+                }
+                else
+                {
+                    if (componentsToRemove.Any(item => string.Equals(item, 
+                        component.DefinitionName, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        AddComponent(components, component.DefinitionName);
+                    }
+                }
+            }
+        }
+
+        private void AddComponent(List<IComponentMatcher> components, string definitionName)
+        {
+            var item = new ConfigurationComponentMatcher(definitionName);
+            components.Add(item);
+        }
+
+        private class ConfigurationComponentMatcher : IComponentMatcher, IContainerMatcher
+        {
+            private readonly string definitionName;
+
+            public ConfigurationComponentMatcher(string definitionName)
+            {
+                this.definitionName = definitionName;
+            }
+
+            public bool MatchesComponent(IComponent component)
+            {
+                return string.Equals(definitionName, 
+                    component.DefinitionName, StringComparison.OrdinalIgnoreCase);
+            }
+
+            public bool MatchesContainer(IContainer container)
+            {
+                return true;
+            }
+        }
+    }
+}
